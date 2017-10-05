588c2ef64b51ba7e21384b8bcea862c3ca50fbc8
Nicolas Esnis <nicolas.esnis@gmail.com>
2017-10-05 18:33:39 +0200

    worked on profile model

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
 
-  get 'profiles/edit'
+  resource :profile, :only => :show, :as => :current_profile, :type => :current_profile
+  resource :profile, only: [ :edit, :destroy ]
 
-  get 'profiles/update'
 
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  get 'recipes/index'
-
-  get 'recipes/show'
-
   resources :recipes, only: [ :index, :show ]
+
   root to: 'recipes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
