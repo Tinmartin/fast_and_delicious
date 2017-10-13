86517c327741c25ab5b1beb49fcb4397724ead46
Marianne Benkamoun <marianne.benkamoun@gmail.com>
2017-10-13 11:32:03 +0200

    index lookin good

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,12 @@
 class Recipe < ApplicationRecord
-  # include AlgoliaSearch
+  include AlgoliaSearch
 
   has_many :ingredients, through: :doses
   has_many :doses, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  # validates :name, uniqueness: true
+  validates :name, uniqueness: true
   validates :name, presence: true
   mount_uploader :picture, PhotoUploader
 
--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,7 +1,7 @@
 
-# require 'algoliasearch-rails'
-# require 'algoliasearch'
-# require 'rubygems'
+require 'algoliasearch-rails'
+require 'algoliasearch'
+require 'rubygems'
 
 
-# AlgoliaSearch.configuration = {  application_id: 'AMLNOKWPBJ', api_key: ENV["ALGOLIA_SECRET_KEY"] }
+AlgoliaSearch.configuration = {  application_id: 'AMLNOKWPBJ', api_key: ENV["ALGOLIA_SECRET_KEY"] }
