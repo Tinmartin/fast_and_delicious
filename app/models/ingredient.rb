1c3c3b2c63314233fa0130847f57bf8f4412f532
Nicolas Esnis <nicolas.esnis@gmail.com>
2017-10-14 12:22:46 +0200

    changed login appearance

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,11 +5,11 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :doses
 
 
-  # validates :name, uniqueness: true
-  # validates :name, presence: true
-  # validates :picture, presence: true
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :picture, presence: true
 
-  # validates :can_change, presence: true
+  validates :can_change, presence: true
 
 
   mount_uploader :picture, PhotoUploader
