deb1ed71775b7053b43a8b1dcfb904247fafacc6
Martin PARENTI <martin.parenti@hotmail.fr>
2017-12-05 12:00:04 +0100

    heroku update

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -3,5 +3,5 @@ require 'algoliasearch-rails'
 require 'algoliasearch'
 require 'rubygems'
 
-AlgoliaSearch.configuration = { application_id: 'AMLNOKWPBJ', api_key: "e82313332c601def5772ffb10acf9c9b" }
+AlgoliaSearch.configuration = { application_id: 'NT0MM889NI', api_key: "1de60570c9c51ace4f0b6b9fcccb1767" }
 
